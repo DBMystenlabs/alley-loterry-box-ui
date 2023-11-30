d1096505563f824bee2dd2bbab9c6966b27b8870
David Bong <david.bong@mystenlabs.com>
2023-11-30 20:58:47 +0900

    Enhance Search and Download functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,52 @@
-// import { ConnectButton } from "@mysten/dapp-kit";
-// import { Box, Container, Flex, Heading } from "@radix-ui/themes";
-// import { WalletStatus } from "./WalletStatus";
+import React, { useState } from 'react';
 import SearchBar from "./components/SearchBar";
 import './styles/App.css';
 
 const App: React.FC = () => {
-  const handleSearch = (address: string) => {
-    // Implement your search logic here, possibly making an API call
-    console.log('Searching for:', address);
+  const [s3Link, setS3Link] = useState(false);
+  const [isSearching, setIsSearching] = useState(false); // New state to track if a search is in progress
+
+  const handleSearch = async (address: string) => {
+    if(isSearching) return; // Prevent searching if already in progress
+
+    setIsSearching(true); // Disable the search button
+    setS3Link(false);
+    console.log('S3 Link State:before', s3Link);
+    try {
+      const url = new URL('https://4o7ziggfwa7v7265bpdcdd3ua40nogyu.lambda-url.ap-northeast-1.on.aws/');
+      url.searchParams.append('address', address);
+
+      const response = await fetch(url.toString(), {
+        method: 'GET',
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const responseBody = await response.text();
+      const parsedBody = JSON.parse(responseBody); // First parse to get the JSON string
+      const data = JSON.parse(parsedBody.body); // Second parse to get the actual object
+  
+      if (data && data.download_url) {
+        setS3Link(data.download_url); // Update the s3Link with the actual download URL
+      }
+
+
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      // Handle the error appropriately in your UI
+    } finally {
+      setIsSearching(false); // Re-enable the search button after the search is complete or fails
+    }
   };
 
   return (
     <>
       <div className="App">
         <header className="App-header">
-          <SearchBar onSearch={handleSearch} />
+          <SearchBar onSearch={handleSearch} isSearching={isSearching} />
+          {s3Link && <a href={s3Link} download="file.xlsx">Download Excel File</a>}
         </header>
       </div>
     </>
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,30 +1,36 @@
-
+// SearchBar.tsx
 import React, { useState } from 'react';
 
 interface SearchBarProps {
-    onSearch: (address: string) => void;
+  onSearch: (address: string) => void;
+  isSearching: boolean;
 }
 
+const SearchBar: React.FC<SearchBarProps> = ({ onSearch, isSearching }) => {
+  const [address, setAddress] = useState('');
 
-const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
-    const [address, setAddress] = useState('');
+  const handleSearchClick = () => {
+    onSearch(address);
+  };
 
-    const handleSearch = () => {
-        onSearch(address);
-    };
-
-    return (
-        <div className="search-bar">
-            <input
-                type="text"
-                placeholder="Wallet Address"
-                value={address}
-                onChange={(e) => setAddress(e.target.value)}
-            />
-            <button onClick={handleSearch}>Search</button>
-        </div>
-    );
+  return (
+    <div className="search-bar">
+      <input
+        type="text"
+        placeholder="Enter Wallet Address"
+        value={address}
+        onChange={(e) => setAddress(e.target.value)}
+        disabled={isSearching} // Disable input while searching
+      />
+      <button 
+        onClick={handleSearchClick} 
+        disabled={isSearching} // Disable button while searching
+        style={{ opacity: isSearching ? 0.5 : 1 }}
+      >
+        {isSearching ? 'Searching...' : 'Search'}
+      </button>
+    </div>
+  );
 };
 
 export default SearchBar;
-
